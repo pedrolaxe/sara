3a057531e128a6dfed31ad7f0a396798843838fd
pedrolaxe <pedrolaxe@gmail.com>
2016-10-07 16:02:59 -0300

    New Functions and database

--- sara.py
+++ sara.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Name: Sara
-# Version: 0.4a
+# Name: SARA
+# Version: 0.7.2b
 # Author: Pedro Laxe
 # Date: 06/10/2016
 from time import ctime
@@ -10,7 +10,7 @@ import os
 import sys
 from gtts import gTTS
 
-version = "0.4 alpha"
+version = "0.7.2 beta"
 
 def falar(audiostd):
     print audiostd
@@ -18,20 +18,28 @@ def falar(audiostd):
     tts.save("tmpsara.mp3")
     os.system("mpg321 -q tmpsara.mp3")
 
+def savedbfile(dbdata):
+    dbfile = open("database.sara", "w")
+    dbdata = str(dbdata)
+    dbfile.write(dbdata)
+    dbfile.close()
+
 def sara(data):
-    dic = {
-    "olá": "Oi Pedro",
-    "oi": "Oi Pedro",
-    "ola": "Oi Pedro",
+    dicionario = []
+    with open("database.sara", "r") as f:
+        dicfile = eval(f.read())
+        for key in dicfile:
+            if key in data:
+                falar(dicfile[key])
 
-    "estou bem": "fico feliz por estar bem!",
-    "bem": "fico feliz por estar bem!",
-
-    "sara": "Em que posso lhe servir meu senhor?",
-    }
-    for x in dic:
-        if x in data:
-            falar(dic[x])
+    if "aprender" in data:
+        falar("Gosto de Aprender coisas novas!")
+        pergunta = str(raw_input("Pergunta: "))
+        resposta = str(raw_input("Resposta: "))
+        dicfile.update({pergunta: resposta})
+        savedbfile(dicfile)
+        falar("Salvei no meu banco de dados!")
+        falar("Reinicie para ver as mudanças.")
 
     if "versao" in data or "version" in data:
           falar("Estou na versão "+version)
